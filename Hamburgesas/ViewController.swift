a3596b218566ca489fbfd1c09d7674f4f3c3e7e4
Victor Danney Garcia Plaza <vdgp@yahoo.com>
2016-01-02 08:18:49 -0500

    Initial Commit

---
+++ Hamburgesas/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hamburgesas
+//
+//  Created by Victor Danney Garcia Plaza on 1/2/16.
+//  Copyright © 2016 Victor Garcia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
